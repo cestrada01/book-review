f64ce3c6daf96efa0e62cfa6409dc81976f6e5d7
carestrada <cestrada@mendoza.gov.ar>
2024-12-09 22:21:54 -0300

    091224

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -2,9 +2,15 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Book extends Model
 {
-    //
+    use HasFactory;
+
+    public function reviwes(){
+
+        return $this->hasMany(Review::class);
+    }
 }
